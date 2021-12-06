b18d029446f965c978daa299de409444714ec4ee
Denis Obolensky <darthdenismaster@yandex.ru>
2021-12-06 08:58:41 +0300

    Add basic project that can compile file

--- src/SLang.Compiler/Program.cs
+++ src/SLang.Compiler/Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using SLang.CodeGen.IL;
+using SLang.Parser;
+
+if (!args.Any())
+{
+    Console.WriteLine("File not specified");
+    return;
+}
+
+var parser = new Parser();
+var syntaxTree = parser.Parse(File.ReadAllText(args[0]));
+
+var codeGen = new CodeGen();
+
+codeGen.GenerateCode(syntaxTree, "output.dll");
